22bfa3fcc7146d35419f015842c2db5dfb982c7d
Ali Dost Jamali <163766301+AliJamali06@users.noreply.github.com>
2024-11-03 12:14:36 +0500

    updated

--- src/app/components/customer-content.tsx
+++ src/app/components/customer-content.tsx
@@ -17,9 +17,9 @@ export default function CustomerContent() {
             <div className="right-side">
               <h1 className="about">About</h1>
               <p className="dummytext">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                Lorem Ipsum has been the industrys standard dummy text ever since the 1500s
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                It has survived not only five centuries but also the leap into electronic typesetting remaining essentially unchanged.
               </p>
               <button className="read-more-button">
                 READ MORE
